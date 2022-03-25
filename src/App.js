83a27553581267e1bfeed61fd2a5817d8d12a07a
Sajid Rahman <sajidjrahman@icloud.com>
2022-03-25 20:28:01 +0100

    card added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Headphones from './components/Headphones/Headphones';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Headphones></Headphones>
     </div>
   );
 }
---
+++ src/components/Headphones/Headphones.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Headphones.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMusic } from '@fortawesome/free-solid-svg-icons'
+
+const Headphones = () => {
+    return (
+        <div className='headphones-container'>
+            <img src="" alt="" />
+            <div>
+                <h2>Name:</h2>
+                <p>Price</p>
+                <button className='button-add-cart'>
+                    <p>Add to Cart</p>
+                    <FontAwesomeIcon icon={faMusic} />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Headphones;
